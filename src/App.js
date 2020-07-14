e71b2954bd97f08e8908bc40bd43cce14f69d133
radhouani mohaned <radhouanimohanad@gmail.com>
2020-07-14 02:14:53 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import './style.css';
+// class MyHeader extends React.Component {
+//   render() {
+//     return (
+//       <div style={{border:solid `1px` `black` ; max-width:`100vw`}}>  
+ 
+//     	<h1 className={{title red}}>Your name here</h1>  
+ 
+//         	<br>  
+ 
+//             <img src={{/imageInSrc.jpg}} /> <br> 
+ 
+//              	<img src={{/imageInPublic.jpg}} />  
+ 
+//     </div>  
+ 
+//     <video width="320" height="240" controls>  
+ 
+//         <source src="myVideo.mp4" type="video/mp4"> 
+ 
+//     </video> 
+//     );
+//   }
+// }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="style">
+          <h1 className="red">Radhouani Mohaned </h1>
+          <img src="../imageInSrc.jpg" className="imageInSrc" alt="imageInSrc" />
+          <img src="../imageInPublic.jpg" className="imageInPublic" alt="imageInPublic" />
+        </div>
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +47,8 @@ function App() {
         >
           Learn React
         </a>
+
+       
       </header>
     </div>
   );
